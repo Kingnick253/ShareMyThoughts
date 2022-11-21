bac588739cecf21e6edaa1c84ad4472377dd4e28
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-21 13:35:45 -0800

    finished all the routes for the user

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,2 +1,16 @@
 const router = require("express").Router();
+const {getUser,getOneUser, createUser, updateUser, deleteUser, addFriend, removeFriend } = require("../../controllers/userController");
+// get and post route
+router.route("/")
+.get(getUser).post(createUser);
+// find get update and delete user route
+router.route("/:userId")
+  .get(getOneUser)
+  .put(updateUser)
+  .delete(deleteUser);
+// post and delete route friends
+router.route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend);
 
+  module.exports = routers;
