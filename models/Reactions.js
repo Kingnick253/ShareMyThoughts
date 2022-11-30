bdd933c2ec3101fd7ce20db71bbc1e8e92c80c00
Kingnick253 <nicholaswebb2016@gmail.com>
2022-11-30 14:35:14 -0800

    removed reaction schema from thoughts.js and created its own file so I can import types.

---
+++ models/Reactions.js
@@ -0,0 +1,41 @@
+const {Schema, Types } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema(
+    {
+
+        
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280,
+        }, 
+
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+          },
+          username: {
+            type: String,
+            require: true,
+          },
+          
+          createdAt: {
+            type: Date,
+            default: Date.now,
+            get: newDate => moment(newDate).format("MM DD, YYYY"),
+          },
+        },
+          {
+          toJSON: {
+            virtuals: true,
+          },
+          id: false,
+          _id: false,
+        }
+    
+
+);
+module.exports = reactionSchema;
+
+
